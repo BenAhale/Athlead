45adb397f549e99c60781b3c9bacfc96c4041766
Ben Ahale <ahaleben@gmail.com>
2020-11-10 16:06:40 +1100

    Basic styling changes to pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_one :trait
   belongs_to :address, optional: true
   has_one :school
+  has_one :statistic
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
