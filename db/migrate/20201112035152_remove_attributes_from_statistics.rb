8574c654d320ea457d1517030c6fb1843c7fd7ca
Ben Ahale <ahaleben@gmail.com>
2020-11-17 23:01:52 +1100

    Removed bad migration

--- db/migrate/20201112035152_remove_attributes_from_statistics.rb
+++
@@ -1,7 +0,0 @@
-class RemoveAttributesFromStatistics < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :statistics, :fg_percentage
-    remove_column :statistics, :ft_percentage
-    remove_column :statistics, :three_percentage
-  end
-end
